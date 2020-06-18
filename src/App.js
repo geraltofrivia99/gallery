7d880ca8b89b1eb40d9a348f5dddffca02c452cd
geraltofrivia99 <geralt.from.rivi9@yandex.ru>
2020-06-18 21:39:33 +0700

    add

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import { Grid } from './components';
 
 import './App.css';
 
-const data = ((w = 20, h = 15) => {
-  let i = 1;
-  return new Array(h).fill(0).map((_, rowIndex) => {
-    return new Array(w).fill(0).map((_, colIndex) => {
-      return {
-        image: `https://picsum.photos/id/${i++}/300/200`,
-        cell: colIndex,
-        rowNumber: rowIndex
-      };
-    });
-  });
-})();
-
 function App() {
   return (
-    <Grid data={data} />
+    <Grid />
   );
 }
 
